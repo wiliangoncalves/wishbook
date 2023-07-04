3f44dc4bd76f7ad0c9f5674c3f8111b6d7104fe5
wilian gonçalves <wwiliangoncalves@gmail.com>
2023-07-04 16:16:43 -0300

    Testando

--- api/src/database/db.py
+++ api/src/database/db.py
@@ -1,28 +1,62 @@
 from main import app
-
-from tortoise import Tortoise
-
 from tortoise.contrib.fastapi import register_tortoise
+from tortoise import Tortoise
 
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
+
 USER = os.getenv('USER')
 HOST = os.getenv('HOST')
 DATABASE = os.getenv('DATABASE')
 PASSWORD = os.getenv('PASSWORD')
+PORT = os.getenv('PORT')
 
-Tortoise.init_models(["src.models.models"], "models")
 async def init():
     await Tortoise.init(
-        db_url='postgres://{USER}:{PASSWORD}@{HOST}:5432/{DATABASE}',
+        db_url=f"postgres://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}",
         modules={"models": ["src.models.models"]},
     )
 
+    await Tortoise.generate_schemas()
+
+# Inicialize o Tortoise ORM antes de registrar as rotas do FastAPI
+app.add_event_handler("startup", init)
+
 register_tortoise(
     app,
-    db_url=f"postgres://{os.getenv('ROOT')}:{os.getenv('PASSWORD')}@{os.getenv('HOST')}:5432/{os.getenv('DATABASE')}",
+    db_url=f"postgres://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}",
     modules={"models": ["src.models.models"]},
     generate_schemas=True,
     add_exception_handlers=True,
 )
+
+# from main import app
+
+# from tortoise import Tortoise
+
+# from tortoise.contrib.fastapi import register_tortoise
+
+# import os
+# from dotenv import load_dotenv
+# load_dotenv()
+# USER = os.getenv('USER')
+# HOST = os.getenv('HOST')
+# DATABASE = os.getenv('DATABASE')
+# PASSWORD = os.getenv('PASSWORD')
+
+# Tortoise.init_models(["src.models.models"], "models")
+# async def init():
+#     await Tortoise.init(
+#         db_url='postgres://{USER}:{PASSWORD}@{HOST}:5432/{DATABASE}',
+#         modules={"models": ["src.models.models"]},
+#     )
+
+# register_tortoise(
+#     app,
+#     db_url=f"postgres://{os.getenv('ROOT')}:{os.getenv('PASSWORD')}@{os.getenv('HOST')}:5432/{os.getenv('DATABASE')}",
+#     modules={"models": ["src.models.models"]},
+#     generate_schemas=True,
+#     add_exception_handlers=True,
+# )
